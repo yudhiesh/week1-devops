46c7846bd7fcc60f8d215d95f17b1f8e438ddc0d
GowthamKrishna <gowthamaddluri@gmail.com>
2023-11-07 22:08:46 +0000

    added workflow and tests

---
+++ tests/test_calculator.py
@@ -0,0 +1,13 @@
+from src.calculator import add,mul,div,sub
+
+def test_add():
+    assert add(1,1) == 0
+
+def test_sub():
+    assert sub(1,1) == 0
+
+def test_div():
+    assert div(1,1) == 1
+
+def test_mul():
+    assert mul(1,1) == 1       
